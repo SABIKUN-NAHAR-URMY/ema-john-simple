6ab1fcf703704fb5ba3888020067f86d7e3e274a
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-09-25 23:20:05 +0600

    order summary section done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -15,7 +15,6 @@ const Shop = () => {
     },[]);
     
     const handelAddToCart = (product) =>{
-        console.log(product);
         const newCart = [...cart,product];
         setCart(newCart);
     };
